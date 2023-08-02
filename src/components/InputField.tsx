20832932729142de3fc14689f928ff8dbdd5188e
Yohannes Teffera <yohannesahunm7@gmail.com>
2023-08-02 18:06:52 +0300

    Add react todo list app with typescript

---
+++ src/components/InputField.tsx
@@ -0,0 +1,36 @@
+import React, { useRef } from "react";
+import "./styles.css";
+
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e:React.FormEvent) => void;
+}
+
+const InputField : React.FC<Props> = ({todo, setTodo, handleAdd }) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <form 
+     className="input"
+     onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+        
+     }}
+     >
+        <input 
+            type="input" 
+            placeholder='Enter a task' 
+            className="input__box"
+            value={todo}
+            ref={inputRef}
+            onChange={(e) => setTodo(e.target.value)}
+         />
+        <button type='submit' className="input_submit" >Go</button>
+    </form>
+  )
+}
+
+export default InputField 
+
